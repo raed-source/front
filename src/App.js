6588a0bd4bbad2e66c700a355f2ecd804f0d7078
raed ABBAS <raed.abbas@hotmail.fr>
2022-09-10 03:07:05 +0200

    dd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Post from './pages/Post';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Register />
+      <Router>
+        <Route exact path='/register'><Register /></Route>
+        <Route exact path='/login'><Login /></Route>
+        <Route exact path='/post/:id'><Post /></Route>
+        <Route exact path='NotFound'><NotFound /></Route>
+      </Router>
+    </div >
   );
 }
 
---
+++ src/pages/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const NotFound=()=>{
+    return(
+        <div className='notfound'>
+            <h3 className='notfound-text'>Not Found</h3>
+        </div>
+    )
+}
+export default NotFound;
